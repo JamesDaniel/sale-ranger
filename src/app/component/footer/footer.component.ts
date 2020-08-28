2a8454836054d1847fab5bf606ad1230f1eac71d
jmcgarr <jmcgarr@fexco.com>
2020-08-28 23:06:43 +0100

    Added a reference to browser window for angular

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WindowRefService } from '../../service/window-ref/window-ref.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private winRef: WindowRefService) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+    // this.winRef.nativeWindow.alert('hello');
   }
 
 }
